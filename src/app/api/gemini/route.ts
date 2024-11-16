25c70b80a83766ca40b1ef493d087135b0dbd2a5
Parth Agarwal <parthagarwalchitkara@gmail.com>
2024-11-17 00:09:57 +0530

    this is a simple ai app

---
+++ src/app/api/gemini/route.ts
@@ -0,0 +1,44 @@
+import { NextRequest, NextResponse } from "next/server";
+import { google } from "@ai-sdk/google";
+import { generateObject } from "ai";
+import { z } from "zod";
+
+export async function POST(req: NextRequest) {
+  const data = await req.json();
+  const addPrompt =
+    "Krishna Score Calculation: Compute the Krishna Score as the sum of points across all 15 questions, scaled out of 100. Provide a brief description of how the score reflects alignment between A and B. Quiz Breakdown: Summarize the quiz results in 6 points, with each point being a concise analysis of the alignment or disconnect between A and B across the quiz. Each summary must not exceed 15 words. Recommendations: Based on the alignment between A and B’s answers, generate 5 personalized recommendations to improve alignment or address areas of disagreement. Indicate whether each recommendation is actionable, reflective, or awareness-building.";
+  // const prompt = `${data.prompt.toString()} Can you analyze the above data based on the below prompt ${addPrompt}`;
+  const prompt = data.prompt.toString();
+
+  // const result = await generateObject({
+  //   model: google("models/gemini-1.5-pro-latest"),
+  //   prompt: prompt,
+  //   schema: z.object({
+  //     krishnaScore: z.number(),
+  //     KrishnaScoreDescription: z.string(),
+  //     quizBreakdown: z.array(z.string().max(100)),
+  //     recommendations: z.array(
+  //       z.object({
+  //         text: z.string(),
+  //         type: z.enum(["actionable", "reflective", "awareness-building"]),
+  //       })
+  //     ),
+  //   }),
+  // });
+
+  const result = await generateObject({
+    model: google("models/gemini-1.5-pro-latest"),
+    prompt: prompt,
+    schema: z.object({
+      headline: z.string(),
+      response: z.string(),
+    }),
+  });
+
+  console.log(result.object);
+
+  return NextResponse.json({
+    message: "This is working",
+    data: result.object,
+  });
+}
